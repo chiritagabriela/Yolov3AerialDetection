d6a230ee202766ec3370edd7b082bc43d7a93f51
Chirita Gabriela <48428603+chiritagabriela@users.noreply.github.com>
2021-04-09 16:25:47 +0300

    Update algoritm :currency_exchange:

--- yolov3.py
+++ yolov3.py
@@ -1,100 +1,92 @@
-# import the necessary packages
 import numpy as np
 import argparse
 import time
 import cv2
 import os
+import requests
 
-# construct the argument parse and parse the arguments
+
+count = 0
+video = cv2.VideoCapture("videoplayback.mp4")
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to input image")
 ap.add_argument("-y", "--yolo", required=True, help="base path to YOLO directory")
 ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
 ap.add_argument("-t", "--threshold", type=float, default=0.3, help="threshold when applying non-maxima suppression")
 args = vars(ap.parse_args())
 
-# load the COCO class labels our YOLO model was trained on
 labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
 
-# initialize a list of colors to represent each possible class label
 np.random.seed(42)
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 
 
-# derive the paths to the YOLO weights and model configuration
 weightsPath = os.path.sep.join([args["yolo"], "yolov3_training_last.weights"])
 configPath = os.path.sep.join([args["yolo"], "yolov3_testing.cfg"])
 
-# load our YOLO object detector trained on COCO dataset (80 classes)
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
-image = cv2.imread(args["image"])
-(H, W) = image.shape[:2]
+while video.isOpened():
 
-# determine only the *output* layer names that we need from YOLO
-ln = net.getLayerNames()
-ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    ret, image = video.read()
+    count = count + 1
+    if(count % 15 == 0):
+        (H, W) = image.shape[:2]
 
-# construct a blob from the input image and then perform a forward
-# pass of the YOLO object detector, giving us our bounding boxes and
-# associated probabilities
-blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-net.setInput(blob)
-start = time.time()
-layerOutputs = net.forward(ln)
-end = time.time()
+        ln = net.getLayerNames()
+        ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-# show timing information on YOLO
-print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+        blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+        net.setInput(blob)
+        start = time.time()
+        layerOutputs = net.forward(ln)
+        end = time.time()
 
-# class IDs, respectively
-boxes = []
-confidences = []
-classIDs = []
+        print("[INFO] YOLO took {:.6f} seconds".format(end - start))
 
-for output in layerOutputs:
-	# loop over each of the detections
-	for detection in output:
-		# extract the class ID and confidence (i.e., probability) of
-		# the current object detection
-		scores = detection[5:]
-		classID = np.argmax(scores)
-		confidence = scores[classID]
-		# filter out weak predictions by ensuring the detected
-		# probability is greater than the minimum probability
-		if confidence > args["confidence"]:
-			# scale the bounding box coordinates back relative to the
-			# size of the image, keeping in mind that YOLO actually
-			# returns the center (x, y)-coordinates of the bounding
-			# box followed by the boxes' width and height
-			box = detection[0:4] * np.array([W, H, W, H])
-			(centerX, centerY, width, height) = box.astype("int")
-			# use the center (x, y)-coordinates to derive the top and
-			# and left corner of the bounding box
-			x = int(centerX - (width / 2))
-			y = int(centerY - (height / 2))
-			# update our list of bounding box coordinates, confidences,
-			# and class IDs
-			boxes.append([x, y, int(width), int(height)])
-			confidences.append(float(confidence))
-			classIDs.append(classID)
+        boxes = []
+        confidences = []
+        classIDs = []
 
-idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["threshold"])
+        for output in layerOutputs:
+            for detection in output:
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                confidence = scores[classID]
+                if confidence > args["confidence"]:
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY, width, height) = box.astype("int")
 
-	# ensure at least one detection exists
-if len(idxs) > 0:
-	# loop over the indexes we are keeping
-	for i in idxs.flatten():
-		# extract the bounding box coordinates
-		(x, y) = (boxes[i][0], boxes[i][1])
-		(w, h) = (boxes[i][2], boxes[i][3])
-		# draw a bounding box rectangle and label on the image
-		#color = [int(c) for c in COLORS[classIDs[i]]]
-		cv2.rectangle(image, (x, y), (x + w, y + h), (255,0,0), 2)
-		text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
-		cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
-# show the output image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
+
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
+
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["threshold"])
+
+        if len(idxs) > 0:
+
+            for i in idxs.flatten():
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+                cv2.rectangle(image, (x, y), (x + w, y + h), (255,0,0), 2)
+                text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+                cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
+                url = "http://172.20.10.2:8888/communication/upload/"
+
+                files = {'files': open('2.PNG', 'rb')}
+                headers = {}
+                response = requests.post(url, files=files, headers=headers)
+        cv2.imshow("video", image)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+video.release()
+cv2.destroyAllWindows()
+
+
+
+
+
